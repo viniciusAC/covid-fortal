ab7497e6a6c787b31a3cf4a25b4fad59745bc364
Vinicius Almeida <vini-fortal@hotmail.com>
2021-04-26 12:41:23 -0300

    funções basicas

---
+++ app.py
@@ -0,0 +1,78 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import datetime
+
+from funcoes_auxiliares.graficos import *
+
+data = pd.read_csv('Base de dados/dados_limpos.csv', sep=';')
+data = data.drop(['Unnamed: 0'], axis=1)
+data['dataCaso'] = pd.to_datetime(data['dataCaso'])
+
+data2 = pd.read_csv('Base de dados/dados_limpos_full.csv', sep=';')
+data2 = data2.drop(['Unnamed: 0'], axis=1)
+data2['dataCaso'] = pd.to_datetime(data2['dataCaso'])
+
+bairro_info = pd.read_csv(f'Base de dados/dados_bairros.csv', sep=',')
+bairro_info.Bairros	= bairro_info.Bairros.str.upper()
+bairro_info = bairro_info.set_index('Bairros')
+
+base = datetime.date.today()
+date_list = [base - datetime.timedelta(days=x) for x in range(1,61)]
+date_list = pd.to_datetime(date_list, errors = 'coerce')
+
+########################################################################################################
+
+st.sidebar.title('Menu')
+pagina_atual = st.sidebar.selectbox('Selecione o tipo de analise', ['Analise geral', 'Analise por bairro', 'Analise por IDH'])
+
+dataAnalise = [datetime.datetime(2020, 1, 1), datetime.date.today()]
+dataAnalise[0] = st.sidebar.date_input('Data de inicio', dataAnalise[0], datetime.datetime(2020, 1, 1), datetime.date.today())
+dataAnalise[1] = st.sidebar.date_input('Data de termino', dataAnalise[1], dataAnalise[0], datetime.date.today())
+dataAnalise = pd.to_datetime(dataAnalise, errors = 'coerce')
+
+dateList = pd.date_range(dataAnalise[0], dataAnalise[1])
+
+filtroDt = (data.dataCaso >= dataAnalise[0]) & (data.dataCaso <= dataAnalise[1])
+df = data[filtroDt]
+filtroDt = (data2.dataCaso >= dataAnalise[0]) & (data2.dataCaso <= dataAnalise[1])
+df2 = data2[filtroDt]
+
+if pagina_atual == 'Analise geral':
+    st.markdown('# Analise geral')
+    info_basicas(df2)
+    grafico_temporal(df2, dateList)
+    graficos_idade(df2)
+elif pagina_atual == 'Analise por bairro':
+    st.markdown('# Analise por bairro')
+    
+    listBairros = bairro_info.index.tolist()
+    values = st.multiselect('Selecione os bairros', listBairros)    
+
+    if len(values) < 1:
+        values = listBairros
+
+    filtroBairro = df.bairroCaso.isin(values)
+    df_bairros = df[filtroBairro]
+
+    info_basicas(df_bairros)
+    grafico_temporal(df_bairros, dateList)
+    bairro_table(df_bairros)
+    graficos_idade(df_bairros)
+
+elif pagina_atual == 'Analise por IDH':
+    st.markdown('# Analise por IDH')
+    values = st.slider('Selecione o intervalo do IDH', 0.0, 1.0, (0.100, 0.960))
+
+    filtroIdhMin = bairro_info['IDH em 2010[8]'] >= values[0]
+    df_bairrosIdhInfo = bairro_info[filtroIdhMin]
+    filtroIdhMax = df_bairrosIdhInfo['IDH em 2010[8]'] <= values[1]
+    df_bairrosIdhInfo = df_bairrosIdhInfo[filtroIdhMax]
+
+    filtroBairroIdh = df.bairroCaso.isin(df_bairrosIdhInfo.index.to_list())
+    df_bairrosIdh = df[filtroBairroIdh]
+
+    info_basicas(df_bairrosIdh)
+    grafico_temporal(df_bairrosIdh, dateList)
+    bairro_table(df_bairrosIdh)
+    graficos_idade(df_bairrosIdh)
---
+++ funcoes_auxiliares/graficos.py
@@ -0,0 +1,85 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import datetime
+
+def grafico_temporal(dfAtual, date_list):
+    infectadosPorDia = []
+    suspeitosPorDia = []
+    obtosPorDia = []
+    for i in date_list:
+        filtroDia = dfAtual.dataCaso == i
+        dfTempData = dfAtual[filtroDia]
+        infectadosPorDia.append(dfTempData[dfTempData.resultadoFinalExame == 'Positivo'].shape[0])
+        suspeitosPorDia.append(-dfTempData[dfTempData.resultadoFinalExame == 'Suspeito'].shape[0])
+        obtosPorDia.append(dfTempData[dfTempData.obitoConfirmado == 'Verdadeiro'].shape[0])
+
+    GrafDiaInfo = np.array([date_list, infectadosPorDia, suspeitosPorDia])
+    dfGrafDia = pd.DataFrame(GrafDiaInfo.T, columns=["data", "casos confirmados", "casos suspeitos"])
+    dfGrafDia = dfGrafDia.set_index('data')
+
+    GrafObitosDiaInfo = np.array([date_list, obtosPorDia])
+    dfObitoGrafDia = pd.DataFrame(GrafObitosDiaInfo.T, columns=["data", "obitos"])
+    dfObitoGrafDia = dfObitoGrafDia.set_index('data')
+
+    st.markdown('### Mostragem dos casos')
+    st.bar_chart(dfGrafDia)
+
+    st.markdown('### Mostragem dos obitos')
+    st.bar_chart(dfObitoGrafDia)
+
+def bairro_table(dfAtual):
+    dados = []
+    for i in dfAtual.bairroCaso.value_counts().index:
+        filtroBairro = dfAtual.bairroCaso == i
+        dfTemp = dfAtual[filtroBairro]
+        dados.append([i, 
+                    dfTemp[dfTemp.resultadoFinalExame == 'Positivo'].shape[0],
+                    dfTemp[dfTemp.resultadoFinalExame == 'Suspeito'].shape[0],
+                    dfTemp[dfTemp.obitoConfirmado == 'Verdadeiro'].shape[0]])
+    
+    dfGrafBairro = pd.DataFrame(dados, columns=["Bairro", "casos confirmados", "casos suspeitos", "obitos"])
+
+    st.markdown('### Dados dos bairros')
+    st.dataframe(dfGrafBairro)
+
+def graficos_idade(dfAtual):
+    dadosPositivos = []
+    dadosObito = []
+    for i in dfAtual.faixaEtaria.value_counts().index:
+        filtroFaixaEtaria = dfAtual.faixaEtaria == i
+        dfTemp = dfAtual[filtroFaixaEtaria]
+
+        filtro = ((dfTemp.resultadoFinalExame == 'Positivo') )
+        dfPositivo = dfTemp[filtro]
+
+        filtro = ((dfTemp.obitoConfirmado == 'Verdadeiro') )
+        dfObito = dfTemp[filtro]
+
+        dadosPositivos.append([i, 
+                        dfPositivo[dfPositivo.sexoCaso == 'FEMININO'].shape[0],
+                        -dfPositivo[dfPositivo.sexoCaso == 'MASCULINO'].shape[0]])
+        
+        dadosObito.append([i, 
+                        dfObito[dfObito.sexoCaso == 'FEMININO'].shape[0],
+                        -dfObito[dfObito.sexoCaso == 'MASCULINO'].shape[0]])
+    
+    dfGrafPositivo = pd.DataFrame(dadosPositivos, columns=["faixa Etaria", "feminino", "masculino"])
+    dfGrafPositivo = dfGrafPositivo.set_index("faixa Etaria")
+
+    dfGrafObito = pd.DataFrame(dadosObito, columns=["faixa Etaria", "feminino", "masculino"])
+    dfGrafObito = dfGrafObito.set_index("faixa Etaria")
+
+    st.markdown('### Casos por faixa etaria e genero')
+    st.bar_chart(dfGrafPositivo)
+
+    st.markdown('### Obitos por faixa etaria e genero')
+    st.bar_chart(dfGrafObito)
+
+def info_basicas(dfAtual):
+    confirmados = dfAtual[dfAtual.resultadoFinalExame == 'Positivo'].shape[0]
+    obitos = dfAtual[dfAtual.obitoConfirmado == 'Verdadeiro'].shape[0]
+    infos = [[dfAtual.shape[0], confirmados, obitos, (obitos/confirmados)*100], [dfAtual.shape[0], confirmados, obitos, (obitos/confirmados)*100], [dfAtual.shape[0], confirmados, obitos, (obitos/confirmados)*100], [dfAtual.shape[0], confirmados, obitos, (obitos/confirmados)*100]]
+    dfInfo = pd.DataFrame(infos, columns=["Casos noticados", "Casos confirmados", "Obitos", "Letalidade"])
+
+    st.table(dfInfo.head(1))
